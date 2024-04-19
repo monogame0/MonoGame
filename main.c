4ea8824ca254f0fe61994e31f765acc425f9e67e
the chen <v54188abcde@outlook.com>
2024-04-20 06:13:37 +0800

    新增了计算自然对数的底

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <stdbool.h>
+#define Secoud_pi 1
 void _print(const char* str, int ms) {
    int count = 0;
    while(*(str + count) != '\0')
@@ -18,7 +19,7 @@ void endl(int n)
         printf("\n");
     }
 }
-void calculate_pi(void)
+void calculate_pi(void)  //计算圆周率
 {
     int Mark = 1;
     double pi = 0.0;
@@ -67,8 +68,61 @@ void calculate_pi(void)
        }
     }
 }
+double exp(double x, int y) //计算x的y次方
+{
+    double _return = 1.0;
+    while(y > 0)
+    {
+        _return *= x;
+        y--;
+    }
+    return _return;
+}
+void calculate_log(void)   //计算自然对数的底e
+{
+    double e;
+    int end, ms, count;
+    bool quite = false;  //是否显示详细信息
+    printf("请输入终止迭代次数：");
+    scanf("%d", &end);
+    printf("请输入每次迭代间隔时间(ms)：");
+    scanf("%d", &ms);
+    while(count <= end)
+    {
+        e = exp(1.0 + 1.0 / (double)count, count);
+        if(GetKeyState(' ') < 0)
+        {
+            if(quite == false)
+            {
+                quite = true;
+            }
+            else
+            {
+                quite = false;
+            }
+            Sleep(10);
+            endl(1);
+        }
+        if(quite == false)
+        {
+            printf("%lf\r", e);   //减少计算详细信息输出所用时间
+        }
+        else
+        {
+            printf("e计算结果: %lf   迭代次数：%d   计算进度: %lf %%  计算中...\r", e, count, (double)count / (double)end * 100.0);
+        }
+        count++;
+        Sleep(ms);
+        if(GetKeyState('L') < 0)
+        {
+            return;
+        }
+    }
+    printf("e计算完成！\n");
+}
+    
 int main() {
-    calculate_pi();
+    calculate_log();
     endl(1);
     _print("Hello, world!\n", 200);
     endl(2);
